b2253760b50e2e592d3bb66a3a1ff8f633ec3cf1
Jacob Miske <jacobmiske@gmail.com>
2025-03-31 13:41:45 -0400

    start of repo

---
+++ main.py
@@ -0,0 +1,30 @@
+# ME 5661 Composite Materials
+# Jacob Miske
+# VTP Composites
+
+class Composite():
+
+    def __init__(self):
+        self.PR12 = 0.1 # no dimension
+        self.
+        self.E1 = 10 # MPa
+        self.E2 = 10 # MPa
+        self
+        return 0
+
+
+def get_longitudinal_modulus():
+    """
+
+    :return:
+    """
+    return 0
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
